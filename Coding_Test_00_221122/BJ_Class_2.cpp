9be8ea26a0d43ffb8e289982083f9bfb811a6247
HunAlexander <multi.function81@gmail.com>
2023-02-22 15:23:52 +0900

    add BJ 10773

--- Coding_Test_00_221122/BJ_Class_2.cpp
+++ Coding_Test_00_221122/BJ_Class_2.cpp
@@ -1,5 +1,5 @@
 #include "Default.h"
-#define _PROBLEM_NUMBER 7568
+#define _PROBLEM_NUMBER 10773
 
 /* == template ==
 
@@ -2195,6 +2195,78 @@ int Solution_7568_00()
 		cout << pPerson->iRank << ' ';
 	}
 	cout << '\n';
+
+	return 0;
+}
+
+#pragma endregion
+
+#pragma region 10773 : 제로
+
+/*
+문제
+나코더 기장 재민이는 동아리 회식을 준비하기 위해서 장부를 관리하는 중이다.
+재현이는 재민이를 도와서 돈을 관리하는 중인데, 애석하게도 항상 정신없는 재현이는 돈을 실수로 잘못 부르는 사고를 치기 일쑤였다.
+재현이는 잘못된 수를 부를 때마다 0을 외쳐서, 가장 최근에 재민이가 쓴 수를 지우게 시킨다.
+재민이는 이렇게 모든 수를 받아 적은 후 그 수의 합을 알고 싶어 한다. 재민이를 도와주자!
+
+입력
+첫 번째 줄에 정수 K가 주어진다. (1 ≤ K ≤ 100,000)
+이후 K개의 줄에 정수가 1개씩 주어진다. 정수는 0에서 1,000,000 사이의 값을 가지며, 정수가 "0" 일 경우에는 가장 최근에 쓴 수를 지우고, 아닐 경우 해당 수를 쓴다.
+정수가 "0"일 경우에 지울 수 있는 수가 있음을 보장할 수 있다.
+
+출력
+재민이가 최종적으로 적어 낸 수의 합을 출력한다. 최종적으로 적어낸 수의 합은 231-1보다 작거나 같은 정수이다.
+*/
+
+/*
+예제 입/출
+4
+3
+0
+4
+0
+//
+0
+
+10
+1
+3
+5
+4
+0
+0
+7
+0
+0
+6
+//
+7
+*/
+
+int Solution_10773()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	int K, iSum = 0;
+	stack<int> st;
+	cin >> K;
+	for (int i = 0; i < K; i++)
+	{
+		int iInput = 0;
+		cin >> iInput;
+		if (0 == iInput)
+			st.pop();
+		else
+			st.push(iInput);
+	}
+	
+	while (!st.empty())
+	{
+		iSum += st.top();
+		st.pop();
+	}
+
+	cout << iSum << '\n';
 
 	return 0;
 }
