36b627f1e190b9dab9790614c4571c0f96e7c777
HunAlexander <multi.function81@gmail.com>
2023-02-26 14:32:58 +0900

    add BJ 18111

--- Coding_Test_00_221122/BJ_Class_2.cpp
+++ Coding_Test_00_221122/BJ_Class_2.cpp
@@ -1,5 +1,5 @@
 #include "Default.h"
-#define _PROBLEM_NUMBER 15829
+#define _PROBLEM_NUMBER 18111
 
 /* == template ==
 
@@ -24,6 +24,7 @@ int Solution_0000()
 #define MEndl '\n'
 #define MPow(a) ((a) * (a))
 #define MOD 1234567891
+#define MAX_HEIGHT 256
 
 #pragma region Previous Solutions
 
@@ -2355,6 +2356,125 @@ int Solution_15829()
 #pragma endregion
 
 #pragma endregion // Previous Solutions
+
+#pragma region 18111 : 마인크래프트
+
+/*
+문제
+팀 레드시프트는 대회 준비를 하다가 지루해져서 샌드박스 게임인 ‘마인크래프트’를 켰다.
+마인크래프트는 1 × 1 × 1(세로, 가로, 높이) 크기의 블록들로 이루어진 3차원 세계에서 자유롭게 땅을 파거나 집을 지을 수 있는 게임이다.
+
+목재를 충분히 모은 lvalue는 집을 짓기로 하였다. 하지만 고르지 않은 땅에는 집을 지을 수 없기 때문에 땅의 높이를 모두 동일하게 만드는 ‘땅 고르기’ 작업을 해야 한다.
+
+lvalue는 세로 N, 가로 M 크기의 집터를 골랐다. 집터 맨 왼쪽 위의 좌표는 (0, 0)이다. 우리의 목적은 이 집터 내의 땅의 높이를 일정하게 바꾸는 것이다.
+우리는 다음과 같은 두 종류의 작업을 할 수 있다.
+
+좌표 (i, j)의 가장 위에 있는 블록을 제거하여 인벤토리에 넣는다. - 2초
+인벤토리에서 블록 하나를 꺼내어 좌표 (i, j)의 가장 위에 있는 블록 위에 놓는다. - 1초
+1번 작업은 2초가 걸리며, 2번 작업은 1초가 걸린다.
+밤에는 무서운 몬스터들이 나오기 때문에 최대한 빨리 땅 고르기 작업을 마쳐야 한다.
+‘땅 고르기’ 작업에 걸리는 최소 시간과 그 경우 땅의 높이를 출력하시오.
+
+단, 집터 아래에 동굴 등 빈 공간은 존재하지 않으며, 집터 바깥에서 블록을 가져올 수 없다.
+또한, 작업을 시작할 때 인벤토리에는 B개의 블록이 들어 있다. 땅의 높이는 256블록을 초과할 수 없으며, 음수가 될 수 없다.
+
+입력
+첫째 줄에 N, M, B가 주어진다. (1 ≤ M, N ≤ 500, 0 ≤ B ≤ 6.4 × 10^7)
+
+둘째 줄부터 N개의 줄에 각각 M개의 정수로 땅의 높이가 주어진다.
+(i + 2)번째 줄의 (j + 1)번째 수는 좌표 (i, j)에서의 땅의 높이를 나타낸다.
+땅의 높이는 256보다 작거나 같은 자연수 또는 0이다.
+
+출력
+첫째 줄에 땅을 고르는 데 걸리는 시간과 땅의 높이를 출력하시오. 답이 여러 개 있다면 그중에서 땅의 높이가 가장 높은 것을 출력하시오.
+
+*/
+
+/*
+입력 예제
+3 4 99
+0 0 0 0
+0 0 0 0
+0 0 0 1
+
+출력
+2 0
+
+입력
+3 4 1
+64 64 64 64
+64 64 64 64
+64 64 64 63
+
+출력
+1 64
+*/
+
+int Solution_18111()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL);
+
+	int N, M, B;
+	int ground[500][500] = { 0, };
+	int iHeight = 0;
+	int iDuration = INT_MAX;
+
+	cin >> N >> M >> B;
+
+	for (int n = 0; n < N; n++)
+	{
+		for (int m = 0; m < M; m++)
+			cin >> ground[n][m];
+	}
+
+	for (int i = 0; i <= MAX_HEIGHT; i++)
+	{
+		int iCurrentDuration = 0;
+		int iCurrentB = B;
+
+		for (int n = 0; n < N; n++)
+		{
+			for (int m = 0; m < M; m++)
+			{
+				if (i == ground[n][m])
+					continue;
+				// 파내는 작업
+				else if (i < ground[n][m])
+				{
+					int iHeightDif = ground[n][m] - i;
+					iCurrentDuration += iHeightDif * 2;
+					iCurrentB += iHeightDif;
+				}
+				// 쌓는 작업
+				else
+				{
+					int iHeightDif = i - ground[n][m];
+					iCurrentDuration += iHeightDif;
+					iCurrentB -= iHeightDif;
+				}
+			}
+		}
+
+		if (0 > iCurrentB)
+			continue;
+
+		if (iDuration > iCurrentDuration)
+		{
+			iDuration = iCurrentDuration;
+			iHeight = i;
+		}
+		else if (iDuration == iCurrentDuration)
+		{
+			iHeight = max(iHeight, i);
+		}
+	}
+
+	cout << iDuration << ' ' << iHeight << '\n';
+
+	return 0;
+}
+
+#pragma endregion
 
 int main()
 {
